ef4f679e7ae18e814967e2b9835f494f4ad69b7e
Michael Wang Shouyang <michaelwang502@yahoo.com>
2021-06-23 00:11:50 -0700

    Update co_occurrence_network.py

--- co_occurrence_network.py
+++ co_occurrence_network.py
@@ -1,4 +1,3 @@
-#%%
 import networkx as nx
 import matplotlib.pyplot as plt
 from nltk.corpus import stopwords
@@ -7,37 +6,62 @@ from nltk.tokenize import sent_tokenize, TweetTokenizer
 from string import punctuation
 from pathlib import Path
 
+#USER DEFINED VARIABLES 
+URL = Path(r"./data/from_russia_w_love.txt") # location of the text document 
 
-def main():
-    file_lower = open_file_to_lower(URL)
-    G = clean_network(file_lower)
-    G_sub = G.subgraph(USE_INSTEAD)  # final_list(G)
-    plot_G(G_sub)
+#the higher this number is, the fewer the number of nodes
+# If this number is set to low, you would only see a hairball 
+ZOOM_IN = 20 
 
+#instead of letting the script find key words for you
+# you can define your own key words here 
+# the user-defined key words will be plotted instead
+USE_INSTEAD = [
+    "secret",
+    "service",
+    "bond",
+    "general",
+    "colonel",
+    "g",
+    "rosa",
+    "klebb",
+    "comrade",
+    "kerim",
+    "james",
+    "tatiana",
+    "romanova",
+    "red",
+    "grant",
+    "kronsteen",
+    "q",
+]
+CASE_USE_INSTEAD = list(map(lambda x: x.title(), USE_INSTEAD))
 
-def back_up_draw_method():
-    G = nx.Graph()
-    with open(URL, "r") as file_obj:
-        for i, row in enumerate(file_obj):
-            row = row.strip().split()
-            if len(row) not in (0, 1):
-                self_node = row[0]
-                for neighbors in row[1:]:
-                    G.add_edge(self_node, neighbors)
-            elif len(row) == 1:
-                if row[0] not in G:
-                    nx.add_node(row[0])
-            else:
-                print(f"row {i+1} is empty")
+def open_file_to_lower(url):
+    """the function reads in a file and returns a lower case string
 
-    nx.draw_networkx(G)
-    plt.gca().margins(0.15, 0.15)
-    plt.show()
+    Args:
+        url ([file path]): [the location of the text document]
 
-    nx.write_adjlist(G, "out.adjlist")
+    Returns:
+        [string]: [a string of text]
+    """
 
+    with open(url, "r", encoding="utf-8") as file_obj:
+        text_string = file_obj.read().lower()
+    return text_string
 
 def final_list(G):
+    """[summary]
+    Note that this method is obsolete if you use the user-defined 
+    variable `USE_INSTEAD`
+
+    Args:
+        G ([a graph object]): a complete network 
+
+    Returns:
+        [set]: a list of key words based on their vertex degrees
+    """
     sorted_list_of_tuples = sorted(
         G.edges(data=True), key=lambda x: x[2]["count"], reverse=True
     )
@@ -48,15 +72,17 @@ def final_list(G):
         second_element.append(element[1])
     return set(first_element + second_element)
 
-
-def open_file_to_lower(url):
-
-    with open(url, "r", encoding="utf-8") as file_obj:
-        text_string = file_obj.read().lower()
-    return text_string
-
-
 def clean_network(str_obj):
+    """the function filters out all stop words and punctuations and 
+    returns a graph object
+    
+
+    Args:
+        str_obj ([string]): [a string of text]
+
+    Returns:
+        [a graph object]: [the cleaned graph version of the string object]
+    """
     G = nx.Graph()
     ignore_words = set(stopwords.words("english")) | set(punctuation)
     ignore_words = ignore_words | set(["“", "”", "‘", "’", "—"])
@@ -82,6 +108,11 @@ def clean_network(str_obj):
 
 
 def plot_G(G):
+    """The function draws a graph object
+
+    Args:
+        G ([graph]): A networkX Graph object
+    """
     counts = [G.edges[edge]["count"] for edge in G.edges]
     pos = nx.spring_layout(G)
 
@@ -112,29 +143,36 @@ def plot_G(G):
     plt.tight_layout()
     plt.show()
 
+def back_up_draw_method():
+    """[summary]
+    This method is obsolete. I included it here for the sake of completeness
+    The method draws an unweighted network 
+    """
+    G = nx.Graph()
+    with open(URL, "r") as file_obj:
+        for i, row in enumerate(file_obj):
+            row = row.strip().split()
+            if len(row) not in (0, 1):
+                self_node = row[0]
+                for neighbors in row[1:]:
+                    G.add_edge(self_node, neighbors)
+            elif len(row) == 1:
+                if row[0] not in G:
+                    nx.add_node(row[0])
+            else:
+                print(f"row {i+1} is empty")
 
-URL = Path(r"./data/from_russia_w_love.txt")
-ZOOM_IN = 20
-USE_INSTEAD = [
-    "secret",
-    "service",
-    "bond",
-    "general",
-    "colonel",
-    "g",
-    "rosa",
-    "klebb",
-    "comrade",
-    "kerim",
-    "james",
-    "tatiana",
-    "romanova",
-    "red",
-    "grant",
-    "kronsteen",
-    "q",
-]
-CASE_USE_INSTEAD = list(map(lambda x: x.title(), USE_INSTEAD))
+    nx.draw_networkx(G)
+    plt.gca().margins(0.15, 0.15)
+    plt.show()
+
+    nx.write_adjlist(G, "out.adjlist")
+
+def main():
+    file_lower = open_file_to_lower(URL)
+    G = clean_network(file_lower)
+    G_sub = G.subgraph(USE_INSTEAD)  # final_list(G)
+    plot_G(G_sub)
 
 if __name__ == "__main__":
     main()
